c6aa36fade2cbf2ac6af2e86e3e040d7d6d06af9
Thomas Morita <>
2020-10-03 03:08:18 -0400

    refine styles

--- src/components/content-card.js
+++ src/components/content-card.js
@@ -8,7 +8,7 @@ export const ContentCard = ({ img, imgPosition = 'left',  headline, body }) => {
 				{imgPosition === 'left' ? (
 					<div style={{ display: 'flex' }}>
 						<Image src={img} style={{ width: '40vw', margin: 'auto' }} />
-						<div style={{ marginLeft: 20, textAlign: 'center' }}>
+						<div style={{ marginLeft: 20, textAlign: 'center', width:'40vw' }}>
 							<h3>{headline}</h3>
 							<p style={{ color: 'grey' }}>
 								{body}
@@ -17,7 +17,7 @@ export const ContentCard = ({ img, imgPosition = 'left',  headline, body }) => {
 					</div>
 				) : (
 					<div style={{ display: 'flex' }}>
-						<div style={{ marginRight: 20, textAlign: 'center' }}>
+						<div style={{ marginRight: 20, textAlign: 'center', width:'40vw' }}>
 							<h3>{headline}</h3>
 							<p style={{ color: 'grey' }}>
 								{body}
