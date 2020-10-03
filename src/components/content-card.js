24e0137446fd977ae09544f28d64844464ec3cf1
Thomas Morita <>
2020-10-03 10:15:40 -0400

    style updates to work with safari

--- src/components/content-card.js
+++ src/components/content-card.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import { Box, Image, Button } from 'grommet';
 
-export const ContentCard = ({ img, imgPosition = 'left',  headline, body }) => {
+export const ContentCard = ({ img, imgPosition = 'left', headline, body }) => {
 	return (
 		<Box pad="large">
 			<div>
 				{imgPosition === 'left' ? (
-					<div style={{ display: 'flex' }}>
-						<Image src={img} style={{ width: '40vw', margin: 'auto' }} />
-						<div style={{ marginLeft: 20, textAlign: 'center', width:'40vw' }}>
+					<div style={{ padding: 15 }}>
+						<Image
+							src={img}
+							style={{ width: '30vw', padding: 5, marginRight: 10, float: 'left', paddingTop: '10vh' }}
+						/>
+						<div style={{ width: '30vw', padding: 6, float: 'right' }}>
 							<h3>{headline}</h3>
-							<p style={{ color: 'grey' }}>
-								{body}
-							</p>
+							<p style={{ color: 'grey' }}>{body}</p>
 						</div>
 					</div>
 				) : (
-					<div style={{ display: 'flex' }}>
-						<div style={{ marginRight: 20, textAlign: 'center', width:'40vw' }}>
+					<div style={{ padding: 15 }}>
+						<div style={{ width: '30vw', padding: 6, float: 'left', marginRight: 10 }}>
 							<h3>{headline}</h3>
-							<p style={{ color: 'grey' }}>
-								{body}
-							</p>
+							<p style={{ color: 'grey' }}>{body}</p>
 						</div>
-						<Image src={img} style={{ width: '40vw', margin: 'auto' }} />
+						<Image src={img} style={{ width: '30vw', padding: 5, float: 'right', paddingTop: '10vh' }} />
 					</div>
 				)}
 			</div>
